789ee9a6811687f1530547420d00c1536690cf32
Jackey-Sparrow <1109952026@qq.com>
2015-09-24 19:14:53 +0800

    add desktop controller

---
+++ public/www/desktop/controllers/desktop-controller.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Jackey Li on 15/9/24.
+ */
+(function (angular) {
+    'use strict';
+
+    var moduleName = 'desktop';
+
+    angular.module(moduleName)
+        .controller('desktopController',
+        ['$scope',
+            function ($scope) {
+
+            }]);
+})(angular);
