85fb64b0510f6f63c232bc9dab0fc001f3e5234b
Jackey.Sparrow <1109952026@qq.com>
2015-10-02 09:10:02 +0800

    account model

---
+++ models/account.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Jackey Li on 2015/10/2.
+ */
+
+/**
+ *
+ * @param account
+ * @constructor
+ */
+function Account(account){
+    this.chooseDate = account.chooseDate;
+    this.description = account.description;
+    this.type = account.type;
+    this.money = account.money;
+    this.moneyUnit = account.moneyUnit;
+}
+
+module.export = Account;
