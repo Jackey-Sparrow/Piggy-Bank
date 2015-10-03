25419b8e0d8a17d80530af63ab828c4a3b69a59a
Jackey.Sparrow <1109952026@qq.com>
2015-10-03 09:34:00 +0800

    move the translation to controller

--- public/www/desktop/controllers/desktop-controller.js
+++ public/www/desktop/controllers/desktop-controller.js
@@ -8,8 +8,12 @@
 
     angular.module(moduleName)
         .controller('desktopController',
-        ['$scope',
-            function ($scope) {
-
+        ['$scope', '$translate',
+            function ($scope, $translate) {
+                $scope.desktop ={
+                    dash:$translate.instant('desktop.dash'),
+                    chats:$translate.instant('desktop.chats'),
+                    account:$translate.instant('desktop.account')
+                };
             }]);
 })(angular);
